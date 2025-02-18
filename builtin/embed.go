19ca8bd7d641b32731ccce5198769ea7bdc412b8
fabiante <fabiante@users.noreply.github.com>
2025-02-18 12:05:42 +0100

    Add builtin pkg with example data files

---
+++ builtin/embed.go
@@ -0,0 +1,42 @@
+package builtin
+
+import (
+	"embed"
+	"errors"
+	"github.com/fabiante/synotrain/app"
+	"gopkg.in/yaml.v3"
+	"io"
+)
+
+//go:embed files/*
+var fs embed.FS
+
+func Get(path string) ([]byte, error) {
+	return fs.ReadFile("files/" + path)
+}
+
+type synonymResource struct {
+	Synonyms app.SynonymGroup
+}
+
+func UnmarshalSynonymFile(reader io.Reader) ([]app.SynonymGroup, error) {
+	decoder := yaml.NewDecoder(reader)
+
+	var groups []app.SynonymGroup
+
+	for {
+		var res synonymResource
+
+		if err := decoder.Decode(&res); err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			} else {
+				return nil, err
+			}
+		}
+
+		groups = append(groups, res.Synonyms)
+	}
+
+	return groups, nil
+}
